39ad77519179f0f0550026c70a62d5b0ed459e57
Felipe Ribeiro <piporibeiro@gmail.com>
2022-07-25 16:41:36 -0300

    feat/add faq page

--- pages/faq.js
+++ pages/faq.js
@@ -1,41 +1,22 @@
-import Link from '../src/components/Link'
-import { PageTitle } from '../src/components/PageTitle'
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen'
 
-export default function FaqPage({ faq }) {
-  console.log
-  return (
-    <div>
-      <PageTitle title={'Faq'} />
-      <h1>FaqPage</h1>
-      <Link href={'/'}>Ir para a Home</Link>
-      <ul>
-        {faq.map(({ question, answer }) => (
-          <li key={question}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+export default FAQScreen
 
 export async function getStaticProps() {
   const FAQ_API_URL =
-    'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json '
+    'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
   const faq = await fetch(FAQ_API_URL)
-    .then((response) => {
-      return response.json()
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json()
     })
-    .then((response) => {
-      return response
+    .then((resposta) => {
+      return resposta
     })
+
   return {
     props: {
       faq,
     },
-    revalidate: 10,
   }
 }
